48c980b23f3f750d32cd7d333805eeecb7b9cf2e
rosemary <m.j.pourmoghadam@gmail.com>
2022-03-09 20:39:32 +0330

    set all buttons in an arrayList in HomeFragment.kt. menu and overided functions added and set. folder menu added in res and home_menu.xml designed. new strings added in strings.xml

--- app/src/main/java/com/example/mynetflix/HomeFragment.kt
+++ app/src/main/java/com/example/mynetflix/HomeFragment.kt
@@ -1,35 +1,79 @@
 package com.example.mynetflix
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.widget.Button
 import androidx.navigation.fragment.findNavController
 import com.example.mynetflix.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-   lateinit var binding:FragmentHomeBinding
+    lateinit var binding: FragmentHomeBinding
+    val favoriteButtons = ArrayList<Button>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setHasOptionsMenu(true)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding= FragmentHomeBinding.inflate(inflater,container,false)
-        // Inflate the layout for this fragment
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
         binding.buttonFavorite0.setOnClickListener {
-            findNavController().navigate(R.id.action_homeFragment_to_comingSoonFragment)
+            NetflixEnvironment.filmList[0].isFavorite = (!NetflixEnvironment.filmList[0].isFavorite)
+
+            binding.buttonFavorite0.setBackgroundColor(Color.RED)
+            // findNavController().navigate(R.id.action_homeFragment_to_comingSoonFragment)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.home_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.profile -> {
+                findNavController().navigate(R.id.action_homeFragment_to_profileRegistrationFragment)
+                return true
+            }
+            R.id.favorite -> {
+                findNavController().navigate(R.id.action_homeFragment_to_favoriteFragment)
+                return true
+            }
+            R.id.coming_soon -> {
+                findNavController().navigate(R.id.action_homeFragment_to_comingSoonFragment)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun createButtonArray() {
+        favoriteButtons.add(binding.buttonFavorite0)
+        favoriteButtons.add(binding.buttonFavorite1)
+        favoriteButtons.add(binding.buttonFavorite2)
+        favoriteButtons.add(binding.buttonFavorite3)
+        favoriteButtons.add(binding.buttonFavorite4)
+        favoriteButtons.add(binding.buttonFavorite5)
+        favoriteButtons.add(binding.buttonFavorite6)
+        favoriteButtons.add(binding.buttonFavorite7)
+        favoriteButtons.add(binding.buttonFavorite8)
+        favoriteButtons.add(binding.buttonFavorite9)
+        favoriteButtons.add(binding.buttonFavorite10)
+        favoriteButtons.add(binding.buttonFavorite11)
     }
 }
