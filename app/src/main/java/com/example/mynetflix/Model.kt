11535f271af4a8cdb2138be530f9a897dfa18813
rosemary <m.j.pourmoghadam@gmail.com>
2022-03-09 16:44:35 +0330

    round_corner_shape.xml added in drawable for design. fragment_show_user_info.xml designed and tested by another fragments.new action from profile fragment to show user info fragment added in nav_graph. var userInfo type changed to nullable in Model.kt

--- app/src/main/java/com/example/mynetflix/Model.kt
+++ app/src/main/java/com/example/mynetflix/Model.kt
@@ -1,15 +1,19 @@
 package com.example.mynetflix
-object NetflixEnvironment{
-    lateinit var userInfo: UserInfo
-    val filmList=ArrayList<Film>()
-}
-data class Film(var title:String,var imageID:Int)
-{
-    var isFavorite=false
+
+object NetflixEnvironment {
+    var userInfo: UserInfo? = null
+    val filmList = ArrayList<Film>()
 }
 
-data class UserInfo(var name:String, var familyName:String,var emailAddress:String)
-{
-    var phoneNumber:String?=null
-    var username:String?=null
+data class Film(var title: String, var imageID: Int) {
+    var isFavorite = false
+}
+
+class UserInfo(
+    var name: String? = null,
+    var familyName: String? = null,
+    var emailAddress: String? = null
+) {
+    var phoneNumber: String=""
+    var username: String=""
 }
