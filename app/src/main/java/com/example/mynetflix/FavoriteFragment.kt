2b5a735a7863aba54f7eb54fc0ef62d9da3a9f98
rosemary <m.j.pourmoghadam@gmail.com>
2022-03-09 23:21:24 +0330

    fragment_favorite.xml designed.text views and image views added in array in FavoriteFragment.kt for clean code.

--- app/src/main/java/com/example/mynetflix/FavoriteFragment.kt
+++ app/src/main/java/com/example/mynetflix/FavoriteFragment.kt
@@ -5,55 +5,72 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.mynetflix.databinding.FragmentFavoriteBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FavoriteFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FavoriteFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+    lateinit var binding: FragmentFavoriteBinding
+    val imageViewsArray = ArrayList<ImageView>()
+    val textViewsArray = ArrayList<TextView>()
+    var index=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentFavoriteBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FavoriteFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FavoriteFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        createImageViewsArray()
+        createTextViewsArray()
+        if (NetflixEnvironment.userInfo != null) {
+            for (film in NetflixEnvironment.filmList) {
+                if (film.isFavorite)
+                {
+                    imageViewsArray[index].setImageResource(film.imageID)
+                    textViewsArray[index].text=film.title
+                    index++
                 }
             }
+        }
+    }
+
+    fun createImageViewsArray() {
+        imageViewsArray.add(binding.imageViewFav0)
+        imageViewsArray.add(binding.imageViewFav1)
+        imageViewsArray.add(binding.imageViewFav2)
+        imageViewsArray.add(binding.imageViewFav3)
+        imageViewsArray.add(binding.imageViewFav4)
+        imageViewsArray.add(binding.imageViewFav5)
+        imageViewsArray.add(binding.imageViewFav6)
+        imageViewsArray.add(binding.imageViewFav7)
+        imageViewsArray.add(binding.imageViewFav8)
+        imageViewsArray.add(binding.imageViewFav9)
+        imageViewsArray.add(binding.imageViewFav10)
+        imageViewsArray.add(binding.imageViewFav11)
+    }
+    fun createTextViewsArray(){
+        textViewsArray.add(binding.textViewTitle0)
+        textViewsArray.add(binding.textViewTitle1)
+        textViewsArray.add(binding.textViewTitle2)
+        textViewsArray.add(binding.textViewTitle3)
+        textViewsArray.add(binding.textViewTitle4)
+        textViewsArray.add(binding.textViewTitle5)
+        textViewsArray.add(binding.textViewTitle6)
+        textViewsArray.add(binding.textViewTitle7)
+        textViewsArray.add(binding.textViewTitle8)
+        textViewsArray.add(binding.textViewTitle9)
+        textViewsArray.add(binding.textViewTitle10)
+        textViewsArray.add(binding.textViewTitle11)
     }
 }
